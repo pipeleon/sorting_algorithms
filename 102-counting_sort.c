0bc15a1ab36c243e87a374ba77ba1d2b7acf56b6
pipeleon <oflg94@gmail.com>
2022-02-16 13:39:24 -0800

    Tast 102 betty

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -10,38 +10,38 @@
 */
 void counting_sort(int *array, size_t size)
 {
-    int n, i, j, k = array[0], *count, *sorted;
-    
-    if (array == NULL || size < 2)
+	int n, i, j, k = array[0], *count, *sorted;
+
+	if (array == NULL || size < 2)
 		return;
 
-    n = (int)size;
-    for (i = 0; i < n; i++)
-        if (k < array[i])
-            k = array[i];
-    k++;
-    count = malloc(sizeof(int) * k);
-    if (!count)
-        return;
-    sorted = malloc(sizeof(int) * n);
-    if (!sorted)
-        return;
-    for (i = 0; i < n; i++)
-    {
-        j = array[i] - 1;
-        count[j]++;
-    }
-    for (j = 1; j < k; j++)
-        count[j] = count[j] + count[j - 1];
-    for (i = n - 1; i >= 0; i--)
-    {
-        j = array[i] - 1;
-        count[j]--;
-        sorted[count[j]] = array[i];
-    }
-    print_array(count, k);
-    for (i = 0; i < n; i++)
-        array[i] = sorted[i];
-    free(count);
-    free(sorted);
+	n = (int)size;
+	for (i = 0; i < n; i++)
+		if (k < array[i])
+			k = array[i];
+	k++;
+	count = malloc(sizeof(int) * k);
+	if (!count)
+		return;
+	sorted = malloc(sizeof(int) * n);
+	if (!sorted)
+		return;
+	for (i = 0; i < n; i++)
+	{
+		j = array[i] - 1;
+		count[j]++;
+	}
+	for (j = 1; j < k; j++)
+		count[j] = count[j] + count[j - 1];
+	for (i = n - 1; i >= 0; i--)
+	{
+		j = array[i] - 1;
+		count[j]--;
+		sorted[count[j]] = array[i];
+	}
+	print_array(count, k);
+	for (i = 0; i < n; i++)
+		array[i] = sorted[i];
+	free(count);
+	free(sorted);
 }
