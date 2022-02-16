cf590b53efe03349de60b84edc5f0a62e5f5a8ec
pipeleon <oflg94@gmail.com>
2022-02-16 00:00:33 -0500

    Task 100

---
+++ 100-shell_sort.c
@@ -0,0 +1,47 @@
+#include "sort.h"
+/**
+* shell_sort - Sorts an array of integers in ascending
+* order using the Shell sort algorithm
+* @array: array of integers.
+* @size: Size of array
+*/
+
+void shell_sort(int *array, size_t size)
+{
+    int i, j, k, interval, n;
+    int tmp;
+    int gap[] = {0, 1, 4, 13, 40, 121, 364, 1093};
+
+    n = (int)size;
+
+    for (i = 0; ; i++)
+    {
+        if (gap[i] > n)
+        {
+            i--;
+            break;
+        }
+    }
+
+    interval = gap[i];
+    while (i > 0)
+    {
+        for (j = interval; j < n; j++)
+        {
+            tmp = array[j];
+            for (k = j; k >= interval; k = k - interval)
+            {
+                if (tmp < array[k - interval])
+                    array[k] = array[k - interval];
+                else
+                    break;
+            }
+            array[k] = tmp;
+        }
+        print_array(array, size);
+        i--;
+        interval = gap[i];
+    }
+
+    
+}
--- sort.h
+++ sort.h
@@ -31,4 +31,6 @@ void selection_sort(int *array, size_t size);
 
 void quick_sort(int *array, size_t size);
 
+void shell_sort(int *array, size_t size);
+
 #endif
