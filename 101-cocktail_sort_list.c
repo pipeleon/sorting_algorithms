779f2b8538f65cadf24163b113b614a242df585c
pipeleon <oflg94@gmail.com>
2022-02-16 13:39:26 -0500

    Task 101

---
+++ 101-cocktail_sort_list.c
@@ -0,0 +1,67 @@
+#include "sort.h"
+/**
+* cocktail_sort_list - Sorts a doubly linked list of integers in ascending
+* order using the Cocktail sorting algorithm
+* @list: list of integers.
+*/
+
+void cocktail_sort_list(listint_t **list)
+{
+    listint_t *current, *tmp;
+    int swap = 1;
+
+    if (list == NULL || *list == NULL)
+		return;
+
+    while (swap == 1)
+    {
+        swap = 0;
+        for (current = (*list)->next; current; current = current->next)
+        {
+            if (current->prev->n > current->n)
+            {
+                tmp = current->prev;
+                current->prev = tmp->prev;
+			    tmp->prev = current;
+			    tmp->next = current->next;
+			    current->next = tmp;
+			    if (current->prev)
+                {
+                    current->prev->next = current;
+                }
+                else
+                {
+                    *list = current;
+                }
+                if (tmp->next)
+                {
+                    tmp->next->prev = tmp;
+                }
+                print_list(*list);
+                swap = 1;
+                tmp = current;
+            }
+        }
+        if (swap == 0)
+            break;
+        for (current = tmp->prev; current; current = current->prev)
+        {
+            if (current->next->n < current->n)
+            {
+                tmp = current->next;
+                if (current->prev)
+                    current->prev->next = tmp;
+                else
+                    *list = tmp;
+                current->next = tmp->next;
+                if (tmp->next)
+                    tmp->next->prev = current;
+			    tmp->next = current;
+			    tmp->prev = current->prev;
+                current->prev = tmp;
+			    print_list(*list);
+                swap = 1;
+            }
+        }
+    }
+}
