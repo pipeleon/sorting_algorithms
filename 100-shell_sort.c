56757ad719f3815e80ee7b4ce2eae6e493beee2a
pipeleon <oflg94@gmail.com>
2022-02-16 07:19:06 -0800

    Task 100 betty style

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -8,43 +8,41 @@
 
 void shell_sort(int *array, size_t size)
 {
-    int i, j, k, interval, n;
-    int tmp;
-    int gap[] = {0, 1, 4, 13, 40, 121, 364, 1093};
+	int i, j, k, interval, n;
+	int tmp;
+	int gap[] = {0, 1, 4, 13, 40, 121, 364, 1093};
 
-    n = (int)size;
+	n = (int)size;
 
-    if (array == NULL || size < 2)
-        return;
+	if (array == NULL || size < 2)
+		return;
 
-    for (i = 0; ; i++)
-    {
-        if (gap[i] > n)
-        {
-            i--;
-            break;
-        }
-    }
+	for (i = 0; ; i++)
+	{
+		if (gap[i] > n)
+		{
+			i--;
+			break;
+		}
+	}
 
-    interval = gap[i];
-    while (i > 0)
-    {
-        for (j = interval; j < n; j++)
-        {
-            tmp = array[j];
-            for (k = j; k >= interval; k = k - interval)
-            {
-                if (tmp < array[k - interval])
-                    array[k] = array[k - interval];
-                else
-                    break;
-            }
-            array[k] = tmp;
-        }
-        print_array(array, size);
-        i--;
-        interval = gap[i];
-    }
-
-    
+	interval = gap[i];
+	while (i > 0)
+	{
+		for (j = interval; j < n; j++)
+		{
+			tmp = array[j];
+			for (k = j; k >= interval; k = k - interval)
+			{
+				if (tmp < array[k - interval])
+					array[k] = array[k - interval];
+				else
+					break;
+			}
+			array[k] = tmp;
+		}
+		print_array(array, size);
+		i--;
+		interval = gap[i];
+	}
 }
